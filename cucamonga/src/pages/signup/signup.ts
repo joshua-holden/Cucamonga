1b86dbae096ffe6d90f8bf4559feb60b024b9b8e
joshua-holden <joshuaholden@u.boisestate.edu>
2018-03-20 11:18:44 -0600

    input fields, button, some validation

--- cucamonga/src/pages/signup/signup.ts
+++ cucamonga/src/pages/signup/signup.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 /**
  * Generated class for the SignupPage page.
@@ -15,15 +16,25 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private signup : FormGroup;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private formbuilder: FormBuilder) {
+  	this.signup = this.formbuilder.group({
+  		firstname: ['', Validators.compose([Validators.maxLength(32), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+  		lastname: ['', Validators.compose([Validators.maxLength(32), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+  		email: ['', Validators.required],
+  		password: ['', Validators.required],
+  		passwordconfirm: ['', Validators.required],
+  	})
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignupPage');
   }
   
-/* Clicking 'Create Account' only logs event */
-  logEvent(event) {
-  	console.log(event);
+/* Some validation functionality*/
+  submitForm() {
+  	this.submitAttempt = true;
+  	console.log(this.signup.value);
   }
 }
