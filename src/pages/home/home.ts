38412764b94e3ac9bb866a604adea86710da0139
Turner Cruz <turnercruz@u.boisestate.edu>
2018-03-21 16:36:30 -0600

    adding project

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SignupPage } from '../signup/signup';
 
 @Component({
   selector: 'page-home',
@@ -11,4 +12,7 @@ export class HomePage {
 
   }
 
+  openSignup() {
+  	this.navCtrl.push(SignupPage);
+  }
 }
