a1f1cd25a11340e9824811bb65a5aa4b6378b8fb
joshua-holden <joshuaholden@u.boisestate.edu>
2018-04-12 23:00:26 -0600

    Populate test items on browse tab #38, test search functionality #39

--- cucamonga/src/pages/browse/browse.ts
+++ cucamonga/src/pages/browse/browse.ts
@@ -13,13 +13,41 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-browse',
   templateUrl: 'browse.html',
 })
-export class BrowsePage {
+export class BrowsePage{ 
+
+  public items = [];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad BrowsePage');
+ /* https://github.com/ionic-team/ionic/tree/v3/demos/src/searchbar */
+
+
+
+  ngOnInit() {
+    this.setItems();
   }
 
+   setItems() { 
+
+	for (var i = 1; i <= 100; i++) { 
+		var temp = "test"+i;
+    	this.items.push(temp);
+    }
+   }
+
+   filterItems(ev: any) {
+    let val = ev.target.value;
+
+    if (val && val.trim() !== '') {
+      this.items = this.items.filter(function(item) {
+        return item.toLowerCase().includes(val.toLowerCase());
+      });
+    }
+
+  }
+
+  ionViewDidLoad() {
+  	console.log('ionViewDidLoad BrowsePage');
+  }
 }
