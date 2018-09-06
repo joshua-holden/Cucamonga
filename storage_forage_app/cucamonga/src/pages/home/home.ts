fc057e33613390c1d9a43510afca845f7651e13f
joshua-holden <joshuaholden@u.boisestate.edu>
2018-09-06 11:48:26 -0600

    closes #13

--- storage_forage_app/cucamonga/src/pages/home/home.ts
+++ storage_forage_app/cucamonga/src/pages/home/home.ts
@@ -1,31 +1,49 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { SignupPage } from '../signup/signup';
 import {LoginPage} from '../login/login';
 import{AccountPage} from '../account/account';
 import {BrowsetabPage} from '../browsetab/browsetab';
 
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
+  private login : FormGroup;
+  private submitAttempt;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formbuilder: FormBuilder) {
+    this.login = this.formbuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+  })
   }
+
+  // submitForm(){
+  // // this.navCtrl.push(BrowsetabPage);
+  //  this.submitAttempt = true;
+  // }
 
   openSignup() {
-  	this.navCtrl.push(SignupPage);
-  }
-  openLogin() {
-  	this.navCtrl.push(LoginPage);
-  }
-  openAccount(){
-    this.navCtrl.push(AccountPage);
+    this.navCtrl.push(SignupPage);
   }
 
   openDashboard() {
     this.navCtrl.push(BrowsetabPage);
   }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+
+submitForm() {
+  
+  this.submitAttempt = true;
+  console.log(this.login.value);
+}
 }
