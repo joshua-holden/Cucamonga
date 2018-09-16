9fc0073afc4e0ac242c9b3fd075857b0edcb351e
jorahsh <jorahhinman@u.boisestate.edu>
2018-09-16 10:36:55 -0600

    fixing issues with new dependencies

---
+++ storage_forage_ws/storage-forage-ws/src/com/storageforage/data/Posting.java
@@ -0,0 +1,13 @@
+package com.storageforage.data;
+
+public class Posting {
+	private String name;
+	private int cost;
+	private String size;
+	
+	public Posting() {
+		this.name = "empty";
+		this.cost = 0;
+		this.size = "none";
+	}
+}
--- storage_forage_ws/storage-forage-ws/src/com/storageforage/resources/PostingResource.java
+++ storage_forage_ws/storage-forage-ws/src/com/storageforage/resources/PostingResource.java
@@ -1,21 +1,28 @@
 package com.storageforage.resources;
 
+import java.io.IOException;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Singleton
 @Path("postings")
 public class PostingResource {
-
+	
 	@Inject
-	public PostingResource() {}
+	public PostingResource() {
+		
+	}
 	
 	@GET
-	@Produces("text/plain")
-	public String getMessage() {
-		return "Postings!";
+	@Produces("application/json")
+	public Response getMessage() {
+		return Response.ok().build();
 	}
 }
